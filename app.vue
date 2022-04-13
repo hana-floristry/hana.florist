40631296e44f5db70d8752bc4c15846542aba7e1
cheeze2000 <me@cheeze2000.net>
2022-04-14 07:37:52 +0800

    feat: import new fonts

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
 <template>
 	<NuxtPage />
 </template>
+
+<style>
+@font-face {
+	font-display: swap;
+	font-family: "Cormorant SC";
+	src: url(~/assets/font/CormorantSC.ttf);
+}
+
+@font-face {
+	font-display: swap;
+	font-family: "Lexend Deca";
+	font-weight: 300;
+	src: url(~/assets/font/LexendDeca.ttf);
+}
+</style>
