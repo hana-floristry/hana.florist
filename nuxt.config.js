3e329084f75e2b143aa5ebb01404a9cfc0dc98a2
cheeze2000 <me@cheeze2000.net>
2022-04-14 07:35:07 +0800

    feat: path alias

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 import { defineNuxtConfig } from "nuxt3";
+import { resolve } from "path";
 
 export default defineNuxtConfig({
+	alias: {
+		"~": resolve(__dirname)
+	},
 	modules: [
 		"nuxt-windicss"
 	]
