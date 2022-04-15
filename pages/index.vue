cc23e74e668d82a982a8505087148e1b7fcd203b
cheeze2000 <me@cheeze2000.net>
2022-04-15 23:36:59 +0800

    feat: add metadata in head tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
+<script setup>
+useHead({
+	charset: "utf-8",
+	htmlAttrs: {
+		lang: "en"
+	},
+	link: [
+		{ rel: "icon", href: "/favicon.ico" }
+	],
+	meta: [
+		{ name: "description", content: "At HANA Floristry, we are passionate about creating vibrant, colourful floral arrangements for weddings, events and every day. We offer a large selection of unique floral designs suitable for any occasion." },
+		{ name: "theme-color", content: "#FFF8DC" }
+	],
+	title: "HANA Floristry",
+	viewport: "width=device-width, initial-scale=1"
+});
+</script>
+
 <template>
 	<div class="flex justify-center items-center w-screen h-screen">
 		<h1 class="text-3xl underline">
