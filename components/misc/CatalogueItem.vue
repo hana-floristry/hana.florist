952f9b768fbecc5faa0c806d6a509e38ae5c9945
cheeze2000 <me@cheeze2000.net>
2022-04-15 23:44:11 +0800

    fix: missing component file

---
+++ components/misc/CatalogueItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+	item: Object
+});
+
+function formatPrice() {
+	const price = Math.floor(props.item.price / 1000);
+	return `Rp ${price.toLocaleString().replace(/,/g, ".")}`;
+}
+</script>
+
+<template>
+	<div class="flex flex-col gap-1 text-red-800">
+		<img
+			:alt="item.name"
+			class="mb-3 aspect-square"
+			:src="item.image"
+		/>
+		<p class="font-cormorant text-2xl md:text-4xl">
+			{{ item.name }}
+		</p>
+		<p class="font-lexend">
+			{{ formatPrice() }}<sup>{{ (item.price % 1000).toString().padStart(3, "0") }}</sup>
+		</p>
+		<p class="font-lexend">
+			{{ item.description }}
+		</p>
+	</div>
+</template>
