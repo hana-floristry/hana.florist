55fac78eaea840e738f7e95d42e02eaff9f07aa5
cheeze2000 <me@cheeze2000.net>
2022-04-17 20:18:45 +0800

    feat: upload image from payload

--- server/api/items.post.js
+++ server/api/items.post.js
@@ -1,7 +1,16 @@
 import Prisma from "@prisma/client";
+import { resolve } from "path";
+import { writeFileSync } from "fs";
 
 const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
+
+function uploadImage(image) {
+	const buffer = Buffer.from(image.split(",")[1], "base64");
+	const filename = `${Date.now()}.png`;
+	writeFileSync(resolve("public", "uploads", filename), buffer);
+	return resolve("/uploads", filename);
+}
 
 export default defineEventHandler(async event => {
 	if (!event.context.auth) {
@@ -10,8 +19,9 @@ export default defineEventHandler(async event => {
 	}
 
 	const data = await useBody(event);
-	const { categoryId } = data;
+	const { categoryId, image } = data;
 	delete data.categoryId;
+	data.image = uploadImage(image);
 
 	const item = await prisma.item.create({
 		data: {
