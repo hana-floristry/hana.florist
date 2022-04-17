780c44277d5411017bbf2bc7fa6765bfe9aadeb3
cheeze2000 <me@cheeze2000.net>
2022-04-17 20:18:20 +0800

    fix: items not returned upon creation

--- server/api/categories.post.js
+++ server/api/categories.post.js
@@ -4,12 +4,17 @@ const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async event => {
-	const data = await useBody(event);
 	if (!event.context.auth) {
 		event.res.statusCode = 404;
 		return event.res.end();
 	}
 
-	const category = await prisma.category.create({ data });
+	const data = await useBody(event);
+	const category = await prisma.category.create({
+		data,
+		include: {
+			items: true
+		}
+	});
 	return category;
 });
