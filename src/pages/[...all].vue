b3deef2477d1bba710fb1444359facc6599376a0
cheeze2000 <me@cheeze2000.net>
2022-05-26 19:33:53 +0800

    feat: operational hours in homepage

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,3 +1,37 @@
-<script setup>
-window.location.href = "/catalogue";
-</script>
+<template>
+	<div class="flex flex-col items-center py-9 text-lg text-center text-red-800">
+		<p class="mb-3 font-cormorant text-3xl">
+			📍Find Us
+		</p>
+		<p class="p-3 pb-0 font-cormorant text-2xl underline">
+			HANA Floristry
+		</p>
+		<p class="p-3 pt-0 font-lexend">
+			Jl. Boulevard Timur No.28d
+			<br>
+			Komplek Cemara Asri
+			<br>
+			Sumatera Utara 20371, Indonesia
+		</p>
+		<p class="p-3 pb-0 font-cormorant text-2xl underline">
+			Contact
+		</p>
+		<p class="p-3 pt-0 font-lexend">
+			contact@hana.florist
+			<br>
+			08153898399
+		</p>
+		<p class="p-3 pb-0 font-cormorant text-2xl underline">
+			Operating Hours
+		</p>
+		<p class="p-3 pt-0 font-lexend">
+			Mon - Sat
+			<br>
+			09:00 - 18:00
+			<br>
+			Sun
+			<br>
+			09:00 - 14:00
+		</p>
+	</div>
+</template>
