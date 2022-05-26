428a5c134d0b427d3a3369dca3cb70d1d42bafc9
cheeze2000 <me@cheeze2000.net>
2022-05-26 23:00:36 +0800

    refactor: revert columns

--- src/pages/catalogue.vue
+++ src/pages/catalogue.vue
@@ -33,7 +33,7 @@ onMounted(fetchCatalogue);
 				<h1 class="my-9 font-cormorant text-2xl text-center text-red-800">
 					{{ category.description }}
 				</h1>
-				<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-15">
+				<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-6 gap-y-15">
 					<div
 						class="flex flex-col gap-1 text-red-800"
 						:key="item.id"
